1258cf379d72c02b0353dc5a4059150807d2d2e0
Artoym Mokrov <motata_12@mail.ru>
2022-03-10 21:01:53 +0300

    Getting started in the repository; Creat cpp file.

---
+++ Hello_world.cpp
@@ -0,0 +1,14 @@
+// Copyright 2022 AstorBrom <motata_12@mail.ru>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    std::string name;
+    std::cout << "Enter name:";
+    std::cin >> name; //input
+    std::cout << "Hello world from " << name << std::endl;//output
+    return 0;
+}
