d612934270248da4d8ebf1a89fdd7f3a9d23300d
Artoym Mokrov <motata_12@mail.ru>
2022-03-11 04:16:54 +0300

    Fixed mozilla code style.

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -6,7 +6,7 @@
 int main() {
     std::string name;
     std::cout << "Enter name:";
-    std::cin >> name; //input%##!
-    std::cout << "Hello world from " << name << std::endl;//output.546&&*,;:
+    std::cin >> name; //input
+    std::cout << "Hello world from " << name << std::endl;//output
     return 0;
 }
