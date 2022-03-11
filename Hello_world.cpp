b2f23685d1088433d6b195b62dd15c1e15285f8c
Artoym Mokrov <motata_12@mail.ru>
2022-03-11 04:09:16 +0300

    Wrong code style (in mozilla edit).

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -6,7 +6,7 @@
 int main() {
     std::string name;
     std::cout << "Enter name:";
-    std::cin >> name; //input
-    std::cout << "Hello world from " << name << std::endl;//output
+    std::cin >> name; //input%##!
+    std::cout << "Hello world from " << name << std::endl;//output.546&&*,;:
     return 0;
 }
